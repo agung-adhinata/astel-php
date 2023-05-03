4041389928b817098376c4f8fc9bbaac9163a6b0
Agung Adhinata <adhi0asta@gmail.com>
2023-05-04 07:42:10 +0800

    feat db: add error boundary

--- dbconn.php
+++ dbconn.php
@@ -6,6 +6,10 @@ class DbConnect
   {
     $env = parse_ini_file('.env');
     $this->instance = mysqli_connect($env['DB_HOSTNAME'], $env['DB_USER'], $env['DB_PASS'], $env['DB_NAME']);
+    if ($this->instance->connect_errno) {
+      echo 'failed to connect to MySQL, ' . $this->instance->connect_error;
+      exit();
+    }
   }
   function query(string $query)
   {
