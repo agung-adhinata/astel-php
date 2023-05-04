a1229d57e456b536716fe0316de686e83048a100
agung-adhinata <adhi0asta@gmail.com>
2023-05-04 11:37:42 +0800

    doc partial: add comment to method for future reference

--- partial/Template.php
+++ partial/Template.php
@@ -16,9 +16,12 @@ class Template
   {
     return require ROOT_DIR . '/partial/partial_head_headless.php';
   }
+  /**
+   *create head props with title and description depends on global variable `$title` and `$description`. That variable
+   * should be created before calling this method.
+   */
   public static function headlessHeadRaw(): mixed
   {
     return require ROOT_DIR . '/partial/partial_head_headless.php';
   }
 }
-
