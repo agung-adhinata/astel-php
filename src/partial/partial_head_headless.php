2e7e0bd04d85e30c95318248ad34eb8a7a671340
Agung Adhinata <adhi0asta@gmail.com>
2023-05-11 11:21:31 +0800

    fix template: rename variables for match app.

--- src/partial/partial_head_headless.php
+++ src/partial/partial_head_headless.php
@@ -1,7 +1,7 @@
 <title>
   <?= $title ?>
 </title>
-<meta name="description" content=<?= $description ?> />
+<meta name="description" content=<?= $desc ?> />
 <meta content="IE=edge" http-equiv="X-UA-Compatible" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="stylesheet" href="style/reset.css" type="text/css" />
