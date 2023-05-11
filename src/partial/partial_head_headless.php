856a27a55a1b4b37a88198e77f57a6d3dd74b0c9
Agung Adhinata <adhi0asta@gmail.com>
2023-05-11 10:18:57 +0800

    feat: add new fonts

--- src/partial/partial_head_headless.php
+++ src/partial/partial_head_headless.php
@@ -5,3 +5,11 @@
 <meta content="IE=edge" http-equiv="X-UA-Compatible" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="stylesheet" href="style/reset.css" type="text/css" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link
+  href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;400;600;700&family=Nunito+Sans:ital,opsz,wght@0,6..12,400;0,6..12,700;1,6..12,400;1,6..12,700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+  rel="stylesheet" />
+<link rel="stylesheet" href="style/base.css" type="text/css" />
--- src/views/devui.php
+++ src/views/devui.php
@@ -8,13 +8,19 @@
 </head>
 
 <body>
-  <h1>this is h1</h1>
+  <section>
+    <h1 class="reset">this is h1</h1>
+    <p>this is paragraph text, no style required i think
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum at totam nobis rerum. Sint veniam totam quo
+      ducimus pariatur, porro esse molestias fugit impedit repellendus autem reprehenderit perferendis fuga ut.
+    </p>
+  </section>
   <h2>this is h2</h2>
   <h3>this is h3</h3>
   <h4>this is h4</h4>
   <h5>this is h5</h5>
-  <p>this is paragraph text, no style required i think</p>
   <div>plain text</div>
+  <a href="#">links</a>
   <code>Monospace text() </code>
 
 
