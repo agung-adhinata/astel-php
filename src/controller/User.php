633b709883e6e11b5af1e6fe3a5ecdbaed312ba3
Agung Adhinata <adhi0asta@gmail.com>
2023-05-15 23:03:53 +0800

    feat: add more user function

--- src/controller/User.php
+++ src/controller/User.php
@@ -13,4 +13,23 @@ class User
       }
     }
   }
+  function changeName(mysqli $db, string $user_id, string $new_name)
+  {
+    if ($result = $db->query("UPDATE user SET nama = {$new_name} WHERE id LIKE {$user_id}")) {
+      return true;
+    }
+    return false;
+  }
+  function changePassword(mysqli $db, string $user_id, string $old_password, string $new_password)
+  {
+    if ($result = $db->query("SELECT password FROM user WHERE id LIKE {$user_id}")) {
+      $result->fetch_array();
+      if ($result['password'] != $old_password)
+        return false;
+
+      if ($db->query("UPDATE user SET password = {$new_password} WHERE id LIKE {$user_id}"))
+        return true;
+    }
+    return false;
+  }
 }
