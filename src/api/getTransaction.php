677a41b0ce088e803d7f91159a42cf7e2a1efad4
Agung Adhinata <adhi0asta@gmail.com>
2023-06-04 18:48:47 +0800

    add past transaction calculation, add POST date

--- src/api/getTransaction.php
+++ src/api/getTransaction.php
@@ -27,32 +27,63 @@ if (!$pengguna_data or mysqli_num_rows($pengguna_data) < 1) {
 }
 
 $where_clause = "";
+$where_clause_for_past = "";
+$past_where_month_clause = null;
+$past_where_year_clause = null;
 // Main fetching
 if (isset($_GET['date']) or isset($_GET['search']) or isset($_GET['group'])) {
   $query = array();
+  $query2 = array();
   if (isset($_GET['date'])) {
     $temp = explode('-', $_GET['date']);
     $year = (int) $temp[0];
     $month = (int) $temp[1];
+
+    $past_where_month_clause = $month;
+    $past_where_year_clause = $year;
     array_push($query, "MONTH(tanggal) = '{$month}' AND YEAR(tanggal) = '{$year}'");
   }
   if (isset($_GET['search'])) {
     $search = $_GET['search'];
     array_push($query, "nama LIKE '%{$search}%'");
+    array_push($query2, "nama LIKE '%{$search}%'");
   }
   if (isset($_GET['group'])) {
     $group = $_GET['group'];
     array_push($query, "id_grup = '{$group}'");
+    array_push($query2, "id_grup = '{$group}'");
   }
   if (sizeof($query) > 0) {
     $where_clause = join(" AND ", $query);
     $where_clause = $where_clause . ' AND ';
   }
+  if (sizeof($query2) > 0) {
+    $where_clause_for_past = join(" AND ", $query2);
+    $where_clause_for_past = $where_clause_for_past . ' AND ';
+  }
 }
-
+$past_where_month_clause = $past_where_month_clause ?? 'MONTH(NOW())';
+$past_where_year_clause = $past_where_year_clause ?? 'YEAR(NOW())';
 $pengguna_data = $pengguna_data->fetch_array();
 
 $transaction_data = $_db->query("SELECT * FROM transaksi WHERE {$where_clause} id_pengguna = {$pengguna_data['id_pengguna']} ORDER BY tanggal DESC");
+
+$past_in_data = $_db->query("SELECT SUM(jumlah) as transaksi_in FROM transaksi WHERE {$where_clause_for_past} MONTH(tanggal) < '{$past_where_month_clause}' AND YEAR(tanggal) <= '{$past_where_year_clause}' AND tipe_transaksi='income' AND id_pengguna = {$pengguna_data['id_pengguna']}");
+$past_out_data = $_db->query("SELECT SUM(jumlah) as transaksi_out FROM transaksi WHERE {$where_clause_for_past} MONTH(tanggal) < '{$past_where_month_clause}' AND YEAR(tanggal) <= '{$past_where_year_clause}' AND tipe_transaksi='expense' AND id_pengguna = {$pengguna_data['id_pengguna']}");
+
+if (!$past_in_data or !$past_out_data) {
+  $response = [
+    'error' => true,
+    'message' => "Server error: {$_db->error}",
+    'stacktrace' => "{$where_clause}"
+  ];
+  http_response_code(500);
+  echo json_encode($response);
+  exit();
+}
+$past_in_data = $past_in_data->fetch_array();
+$past_out_data = $past_out_data->fetch_array();
+
 $in_data = $_db->query("SELECT SUM(jumlah) as total_in FROM transaksi WHERE {$where_clause} tipe_transaksi='income' AND id_pengguna = {$pengguna_data['id_pengguna']}")->fetch_array();
 $out_data = $_db->query("SELECT SUM(jumlah) as total_out FROM transaksi WHERE {$where_clause} tipe_transaksi='expense' AND id_pengguna = {$pengguna_data['id_pengguna']}")->fetch_array();
 
@@ -71,10 +102,12 @@ if (!$transaction_data or !$in_data or !$out_data) {
     $transactions[] = $row;
   }
   $total = ((int) $in_data["total_in"] ?? 0) - ((int) $out_data["total_out"] ?? 0);
+  $past_total = ((int) $past_in_data['transaksi_in'] ?? 0) - ((int) $past_out_data['transaksi_out'] ?? 0);
   $response = [
     "transactions" => $transactions,
     "total_income" => $in_data["total_in"] ?? 0,
     "total_expense" => $out_data["total_out"] ?? 0,
+    "past_total" => $past_total ?? 0,
     "total" => $total
   ];
   echo json_encode($response);
--- src/api/postTransaction.php
+++ src/api/postTransaction.php
@@ -29,20 +29,32 @@ if (!$pengguna_data or mysqli_num_rows($pengguna_data) < 1) {
 }
 $POST = json_decode(file_get_contents('php://input'), true);
 
-if (!isset($POST['name']) or !isset($POST['value']) or !isset($POST['group']) or !isset($POST['desc']) or !isset($POST['type'])) {
+if (!isset($POST['name']) or !isset($POST['value']) or !isset($POST['group']) or !isset($POST['desc']) or !isset($POST['type']) or !isset($POST['date']) or !isset($POST['time'])) {
   $response = [
     'error' => true,
-    'message' => 'You\'re not a pengguna with ' . $POST['name']
+    'message' => 'missing POST attribute' . $POST['name']
   ];
-  http_response_code(500);
+  http_response_code(400);
   echo json_encode($response);
   exit();
 }
+$datetime = $_db->query("SELECT ADDTIME(CONVERT(DATE('{$POST['date']}'), DATETIME), TIME('{$POST['time']}'))");
+if (!$datetime) {
+  $response = [
+    'error' => true,
+    'message' => 'date attibute mismatch, ' . $POST['date'] . ' ' . $POST['time'],
+    'stacktrace' => $datetime
+  ];
+  http_response_code(400);
+  echo json_encode($response);
+  exit();
+}
+$datetime = $datetime->fetch_array()[0];
 $pengguna_data = $pengguna_data->fetch_array();
 if (isset($POST['id']) && strlen($POST['id']) > 0) {
-  $transaction_data = $_db->query("UPDATE transaksi SET nama = '{$POST['name']}', tipe_transaksi = '{$POST['type']}', jumlah = '{$POST['value']}', deskripsi = '{$POST['desc']}', id_grup = '{$POST['group']}' WHERE id_transaksi = '{$POST['id']}'");
+  $transaction_data = $_db->query("UPDATE transaksi SET nama = '{$POST['name']}', tipe_transaksi = '{$POST['type']}', jumlah = '{$POST['value']}', deskripsi = '{$POST['desc']}', id_grup = '{$POST['group']}', tanggal = '{$datetime}' WHERE id_transaksi = '{$POST['id']}'");
 } else {
-  $transaction_data = $_db->query("INSERT INTO transaksi(id_pengguna, nama, tipe_transaksi, jumlah, deskripsi, id_grup) value ('{$pengguna_data['id_pengguna']}','{$POST['name']}', '{$POST['type']}', '{$POST['value']}','{$POST['desc']}', '{$POST['group']}')");
+  $transaction_data = $_db->query("INSERT INTO transaksi(id_pengguna, nama, tipe_transaksi, jumlah, deskripsi, id_grup,tanggal) value ('{$pengguna_data['id_pengguna']}','{$POST['name']}', '{$POST['type']}', '{$POST['value']}','{$POST['desc']}', '{$POST['group']}','{$datetime}')");
 }
 
 if (!$transaction_data) {
